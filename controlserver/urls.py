8b857298c6780ed5e3c5298c037f3059926995df
Kanik Gupta <kanikgupta46@gmail.com>
2026-09-02 23:19:36 +0530

    Expose standard Django admin route

--- controlserver/urls.py
+++ controlserver/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("automation-admin/", admin.site.urls),
     path("", include("control.urls")),
 ]
